55b200fdbbcb0c9e46b58501481cc12e7f89f1d5
Christine Ekke <k.ekke2906@gmail.com>
2025-05-23 02:43:29 +0300

    alldone

--- src/widget.py
+++ src/widget.py
@@ -1,23 +1,22 @@
-from src.masks import get_mask_account,get_mask_card_number
+from datetime import datetime
+
 
 def mask_account_card(account_card: str) -> str:
-    '''Возвращает строку с замаскированным номером'''
+    """Возвращает строку с замаскированным номером"""
     parts = account_card.split()
     card_type = " ".join(parts[:-1])
     number = parts[-1]
 
-    if 'счет' in card_type.lower():
-        masked_number = '**' + number[-4:]
+    if "счет" in card_type.lower():
+        masked_number = "**" + number[-4:]
     else:
-        masked_number = number[:4] + ' ' + number[4:6] + '** ****' + number[-4:]
+        masked_number = number[:4] + " " + number[4:6] + "** ****" + number[-4:]
 
-    return f'{card_type} {masked_number}'
+    return f"{card_type} {masked_number}"
 
 
-from datetime import datetime
-
-def get_date(date:str) -> str :
+def get_date(date: str) -> str:
     '''возвращает строку с датой в формате "ДД.ММ.ГГГГ"'''
     current_date = datetime.fromisoformat(date)
-    date_formatted = current_date.strftime('%d.%m.%Y')
+    date_formatted = current_date.strftime("%d.%m.%Y")
     return date_formatted

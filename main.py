72894f48141abf794b27e2473af232848e1d2f55
Christine Ekke <k.ekke2906@gmail.com>
2025-05-31 21:53:26 +0300

    homework 10.1 done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from src.widget import get_date, mask_account_card
 from src.processing import filter_by_state, sort_by_date
+from src.widget import get_date, mask_account_card
 
 account_card = input("Введите номер: ")
 date = input("Введите дату: ")
@@ -9,10 +9,10 @@ print(mask_account_card(account_card))
 print(get_date(date))
 
 _opers = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
 print(filter_by_state(_opers))

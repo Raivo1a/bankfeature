e35446dcbf5c7dbd231fb09e689c2707cd2f8e8e
Christine Ekke <k.ekke2906@gmail.com>
2025-05-31 21:03:56 +0300

    add sort by date

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,12 @@
-def filter_by_state(operations, state='EXECUTED'):
-'''Функция возвращает новый список словарей, содержащий только те словари, у которых ключ EXECUTED'''
+def filter_by_state(operations: list[dict], state:str = 'EXECUTED') -> list[dict]:
+    '''Функция возвращает новый список словарей, содержащий только те словари, у которых ключ EXECUTED'''
     result = []
     for operation in operations:
         if operation['state'] == state:
             result.append(operation)
     return result
+
+def sort_by_date(data: list[dict[str]], descending: bool = True) -> list[dict[str]]:
+    '''Функция сортировки списка банковских операций по дате'''
+    sorted_list = sorted(data, key=lambda x: x["date"], reverse=descending)
+    return sorted_list

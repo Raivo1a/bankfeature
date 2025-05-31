3f9837ba069c0bb1090ce259302ba3a630582813
Christine Ekke <k.ekke2906@gmail.com>
2025-06-01 00:02:12 +0300

    fixes homework 10.1 done

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,10 @@
+import typing
+
+
 def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dict]:
-    """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ EXECUTED"""
+    """Функция принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
+    Функция возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state соответствует указанному значению"""
     result = []
     for operation in operations:
         if operation["state"] == state:
@@ -7,7 +12,9 @@ def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dic
     return result
 
 
-def sort_by_date(data: list[dict[str]], descending: bool = True) -> list[dict[str]]:
-    """Функция сортировки списка банковских операций по дате"""
+def sort_by_date(data: list[dict[str, typing.Any]], descending: bool = True) -> list[dict[str, typing.Any]]:
+    """Функция принимает список словарей и необязательный параметр, задающий
+    порядок сортировки (по умолчанию — убывание).
+    Функция должна возвращать новый список, отсортированный по дате (date)"""
     sorted_list = sorted(data, key=lambda x: x["date"], reverse=descending)
     return sorted_list

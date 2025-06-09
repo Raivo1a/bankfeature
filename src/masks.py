c9b51b711edc578ab9d07abbafaf7f64fabba173
Christine Ekke <k.ekke2906@gmail.com>
2025-06-09 15:49:42 +0300

    fixes homework 10.1 done

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,7 @@ from typing import Union
 def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     """Функция прячет номер карты"""
     if len(card_number) != 16:
-        return "Неверный номер карты"
+        raise ValueError('Ошибка! Введите корректный номер карты или счета')
     else:
         return card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
 
@@ -12,6 +12,6 @@ def get_mask_card_number(card_number: Union[str]) -> Union[str]:
 def get_mask_account(card_account: Union[str]) -> Union[str]:
     """Функция прячет номер счета"""
     if len(card_account) != 20:
-        return "Неверный номер счета"
+        raise ValueError('Ошибка! Введите корректный номер карты или счета')
     else:
         return "**" + card_account[-4:]
--- src/widget.py
+++ src/widget.py
@@ -8,18 +8,16 @@ def mask_account_card(account_card: str) -> str:
     parts = account_card.split()
     card_type = " ".join(parts[:-1])
 
-    if len(account_card) <= 0:
-        return "Ошибка! Введите корректный номер карты или счета"
-    elif "счет" in card_type.lower():
-        masked_account_number = f"{account_card[:4]} {get_mask_account(account_card[5:])}"
+    if 'счет' in card_type.lower():
+        masked_account_number = f'{account_card[:4]} {get_mask_account(account_card[5:])}'
         return masked_account_number
     else:
-        masked_card_number = f"{account_card[:-16]}{get_mask_card_number(account_card[-16:])}"
+        masked_card_number = f'{account_card[:-16]}{get_mask_card_number(account_card[-16:])}'
         return masked_card_number
 
 
 def get_date(date: str) -> str:
-    '''возвращает строку с датой в формате "ДД.ММ.ГГГГ"'''
+    '''Возвращает строку с датой в формате "ДД.ММ.ГГГГ"'''
     current_date = datetime.fromisoformat(date)
     date_formatted = current_date.strftime("%d.%m.%Y")
     return date_formatted
